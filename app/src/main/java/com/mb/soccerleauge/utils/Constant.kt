b4d809b7696aee693183d274050c16caa00e1978
Mert Büyük <63505261+mertbuyuk@users.noreply.github.com>
2021-10-11 10:55:58 +0300

    Update Constant.kt

--- app/src/main/java/com/mb/soccerleauge/utils/Constant.kt
+++ app/src/main/java/com/mb/soccerleauge/utils/Constant.kt
@@ -3,6 +3,6 @@ package com.mb.soccerleauge.utils
 object Constant {
 
     const val GET_ALL_TEAMS = "teams"
-    const val API_KEY = "f42f4c99a02648ab9d95cf9baaebf0d7"
+    const val API_KEY = "your-api"
     const val BASE_URL = "https://api.football-data.org/v2/competitions/2002/"
 }
